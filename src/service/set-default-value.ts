f264932cec2915fb4060f53475f1ce12d1cf57c1
Min-Nan Liao <minnan.liao@udngroup.com.tw>
2026-03-24 10:32:44 +0800

    【調整】附值邏輯

--- src/service/set-default-value.ts
+++ src/service/set-default-value.ts
@@ -9,22 +9,22 @@ const HPEnhancePlusNodesEnum = new HPEnhancePlusNodes();
 const ColorNodesEnum = new ColorNodes();
 
 export function setBrightnessDefaultValue() {
-    store.$state.information.nodes[2].selected = store.$state.color.selected;
-    store.$state.information.nodes[2].result = store.$state.color.result;
+    store.$state.information.nodes[2].selected = JSON.parse(JSON.stringify(store.$state.color.result));
+    store.$state.information.nodes[2].result = JSON.parse(JSON.stringify(store.$state.color.result));
 
     const colorResult = store.$state.color.nodes.find(n => n.result == store.$state.color.result);
     
-    store.$state.brightnessPlus.nodes[0].result = colorResult.brightness;
-    store.$state.brightnessPlus.nodes[0].nodes[0].result = colorResult.brightness;
+    store.$state.brightnessPlus.nodes[0].result = JSON.parse(JSON.stringify(colorResult.brightness));
+    store.$state.brightnessPlus.nodes[0].nodes[0].result = JSON.parse(JSON.stringify(colorResult.brightness));
 
-    store.$state.brightnessPlus.nodes[1].selected = colorResult.contrast;
-    store.$state.brightnessPlus.nodes[1].nodes[0].selected = colorResult.contrast;
+    store.$state.brightnessPlus.nodes[1].selected = JSON.parse(JSON.stringify(colorResult.contrast));
+    store.$state.brightnessPlus.nodes[1].nodes[0].selected = JSON.parse(JSON.stringify(colorResult.contrast));
     
-    store.$state.brightnessPlus.nodes[0].selected = colorResult.brightness;
-    store.$state.brightnessPlus.nodes[0].nodes[0].selected = colorResult.brightness;
+    store.$state.brightnessPlus.nodes[0].selected = JSON.parse(JSON.stringify(colorResult.brightness));
+    store.$state.brightnessPlus.nodes[0].nodes[0].selected = JSON.parse(JSON.stringify(colorResult.brightness));
 
-    store.$state.brightnessPlus.nodes[1].result = colorResult.contrast;
-    store.$state.brightnessPlus.nodes[1].nodes[0].result = colorResult.contrast;
+    store.$state.brightnessPlus.nodes[1].result = JSON.parse(JSON.stringify(colorResult.contrast));
+    store.$state.brightnessPlus.nodes[1].nodes[0].result = JSON.parse(JSON.stringify(colorResult.contrast));
     
     // 當 color 是 HP Enhance+ 時 brightness 的 dynamic contrast 為 disable 並且關閉
     if(store.$state.color.result == HPEnhancePlusNodesEnum.result) {
